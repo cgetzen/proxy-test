90ea5460dc0014bf1ae5116d098c555e7cf4e699
Charlie Getzen <charlie@bolt.com>
2022-11-30 18:06:51 -0600

    Enhancement #3

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -32,8 +32,25 @@ resource "kubernetes_deployment" "this" {
         container {
           image = "${local.repo}:${var.tag}"
           name  = local.name
+
+          env_from {
+            config_map_ref {
+              name = "environment"
+            }
+          }
         }
       }
     }
+  }
+}
+
+// Enhancement 3
+resource "kubernetes_config_map" "environment" {
+  metadata {
+    name = "environment"
+  }
+
+  data = {
+    TEMPLATE_DATA = "Hello World."
   }
 }
